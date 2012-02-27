54248ab912f527e0af67184aaef4cdd9c393dc3d
David McNett <nugget@flightaware.com>
2012-02-27 12:59:24 -0600

    Move web stuff to its own namespace for collission management with macnugget_web stuff.

--- packages/ergkeeper/web.tcl
+++ packages/ergkeeper/web.tcl
@@ -1,143 +1,145 @@
 package require uuid
 package require form
 
-proc page_init {} {
-	set ::db [dbconnect DB]
-	unset -nocomplain ::session ::user
+namespace eval ::ergkeeper {
+	proc page_init {} {
+		set ::db [dbconnect ergDB]
+		unset -nocomplain ::session ::user
 
-	load_config
-	array set ::session [get_session]
-}
+		load_config
+		array set ::session [get_session]
+	}
 
-proc page_term {} {
-	dbdisconnect
-	abort_page
-}
+	proc page_term {} {
+		dbdisconnect
+		abort_page
+	}
 
-proc page_head {{title "ErgKeeper"}} {
-	puts "<html>"
-	puts "<head>"
-	puts "<title>$title</title>"
-	puts {<link rel="stylesheet" href="/css/default.css" type="text/css" />}
-	puts {<link rel="shortcut icon" href="/favicon.ico" />}
-	puts "</head>"
-	puts "<body>"
+	proc page_head {{title "ErgKeeper"}} {
+		puts "<html>"
+		puts "<head>"
+		puts "<title>$title</title>"
+		puts {<link rel="stylesheet" href="/css/default.css" type="text/css" />}
+		puts {<link rel="shortcut icon" href="/favicon.ico" />}
+		puts "</head>"
+		puts "<body>"
 
-	puts "<div class=\"header\">"
-	puts "<a href=\"/\"><img class=\"topimage\" border=\"0\" src=\"/images/logo-xparent.png\" /></a>"
+		puts "<div class=\"header\">"
+		puts "<a href=\"/\"><img class=\"topimage\" border=\"0\" src=\"/images/logo-xparent.png\" /></a>"
 
-	set menu {/about "About ErgKeeper" /upload "Upload" /chooser "Post" /privacy "Privacy"}
+		set menu {/about "About ErgKeeper" /upload "Upload" /chooser "Post" /privacy "Privacy"}
 
-	if {[info exists ::user(id)]} {
-		if {[info exists ::rkprofile(small_picture)]} {
-			set img_url $::rkprofile(small_picture)
-		} else {
-			set img_url "/images/userpic.jpg"
+		if {[info exists ::user(id)]} {
+			if {[info exists ::rkprofile(small_picture)]} {
+				set img_url $::rkprofile(small_picture)
+			} else {
+				set img_url "/images/userpic.jpg"
+			}
+			lappend menu "/logout"
+			lappend menu "Logout <span style=\"font-weight: normal;\">(<img class=\"topuser\" height=\"20\" width=\"20\" src=\"$img_url\" /> $::rkprofile(name))</span>"
+
+			#puts "<a href=\"/logout\" class=\"topuser\">Logout <span style=\"font-weight: normal;\">(<img class=\"topuser\" height=\"20\" width=\"20\" src=\"$img_url\" /> $::rkprofile(name))</span></a>"
 		}
-		lappend menu "/logout"
-		lappend menu "Logout <span style=\"font-weight: normal;\">(<img class=\"topuser\" height=\"20\" width=\"20\" src=\"$img_url\" /> $::rkprofile(name))</span>"
 
-		#puts "<a href=\"/logout\" class=\"topuser\">Logout <span style=\"font-weight: normal;\">(<img class=\"topuser\" height=\"20\" width=\"20\" src=\"$img_url\" /> $::rkprofile(name))</span></a>"
+		foreach {uri label} $menu {
+			puts "<a href=\"$uri\" class=\"topmenu\">$label</a> "
+		}
+
+
+		puts "</div>"
+		puts "<div class=\"body\">"
 	}
 
-	foreach {uri label} $menu {
-		puts "<a href=\"$uri\" class=\"topmenu\">$label</a> "
+	proc page_foot {} {
+		puts "</div>"
+
+		if {0} {
+			puts "<p>debug:</p>"
+			foreach a {::session ::user ::rkuser ::rkprofile} {
+				if {[info exists $a]} {
+					parray $a
+				}
+			}
+		}
+
+		# puts "<div class=\"footer\">&copy; Copyright 2012 David C. McNett.  All Rights Reserved.</div>"
+		puts "</body>"
+		puts "</html>"
 	}
 
+	proc load_user {id} {
+		array set ::user {}
 
-	puts "</div>"
-	puts "<div class=\"body\">"
-}
+		if {[info exists id] && $id != "" && [ctype digit $id]} {
+			pg_select $::db "SELECT * FROM users WHERE id = $id" buf {
+				array set ::user [array get buf {[a-z]*}]
 
-proc page_foot {} {
-	puts "</div>"
-
-	if {0} {
-		puts "<p>debug:</p>"
-		foreach a {::session ::user ::rkuser ::rkprofile} {
-			if {[info exists $a]} {
-				parray $a
+				if {[info exists ::user(runkeeper_oauth_token)]} {
+			        lassign [runkeeper_request user] success arrayinfo details
+			        array set ::rkuser $arrayinfo
+			        lassign [runkeeper_request profile] success arrayinfo details
+			        array set ::rkprofile $arrayinfo
+				}
 			}
 		}
 	}
 
-	# puts "<div class=\"footer\">&copy; Copyright 2012 David C. McNett.  All Rights Reserved.</div>"
-	puts "</body>"
-	puts "</html>"
-}
+	proc get_session {} {
+		set session [cookie get ergkeeper_session]
 
-proc page_response {} {
-	uplevel 1 {
-		unset -nocomplain response
-		load_response
-
-		if {![array exists response]} {
-			array set response {}
-		}
-	}
-}
-
-proc load_user {id} {
-	array set ::user {}
-
-	if {[info exists id] && $id != "" && [ctype digit $id]} {
-		pg_select $::db "SELECT * FROM users WHERE id = $id" buf {
-			array set ::user [array get buf {[a-z]*}]
-
-			if {[info exists ::user(runkeeper_oauth_token)]} {
-		        lassign [runkeeper_request user] success arrayinfo details
-		        array set ::rkuser $arrayinfo
-		        lassign [runkeeper_request profile] success arrayinfo details
-		        array set ::rkprofile $arrayinfo
+		if {$session != ""} {
+			pg_select $::db "SELECT * FROM sessions WHERE session = [pg_quote $session]" buf {
+				array set ::session [array get buf {[a-z]*}]
+				load_user $buf(user_id)
+				update_session $session
+				return [array get ::session]
 			}
 		}
+
+		if {![info exists ::session]} {
+			set ins(session)		[::uuid::uuid generate]
+			set ins(source)			[env SERVER_NAME]
+			set ins(referer)		[env HTTP_REFERER]
+			set ins(ip_create)		[env REMOTE_ADDR]
+			set ins(ip_recent)		$ins(ip_create)
+			set ins(agent_create)	[env HTTP_USER_AGENT]
+			set ins(agent_recent)	$ins(agent_create)
+
+			if {[sql_insert_from_array sessions ins]} {
+				cookie set ergkeeper_session $ins(session) -path "/" -days 3650
+			}
+
+			unset -nocomplain ::session
+			pg_select $::db "SELECT * FROM sessions WHERE session = [pg_quote $ins(session)]" buf {
+				return [array get buf {[a-z]*}]
+			}
+		}
+
+		return {session ""}
 	}
-}
 
-proc get_session {} {
-	set session [cookie get ergkeeper_session]
+	proc update_session {id} {
+		set sql "UPDATE sessions SET ip_recent = [pg_quote [env REMOTE_ADDR]], agent_recent = [pg_quote [env HTTP_USER_AGENT]] WHERE session = [pg_quote $id]"
+		sql_exec $::db $sql
+	}
 
-	if {$session != ""} {
-		pg_select $::db "SELECT * FROM sessions WHERE session = [pg_quote $session]" buf {
-			array set ::session [array get buf {[a-z]*}]
-			load_user $buf(user_id)
-			update_session $session
-			return [array get ::session]
+	proc require_login {} {
+		if {![info exists ::user(id)]} {
+			puts [runkeeper_login_button]
+			page_foot
+			page_term
 		}
 	}
 
-	if {![info exists ::session]} {
-		set ins(session)		[::uuid::uuid generate]
-		set ins(source)			[env SERVER_NAME]
-		set ins(referer)		[env HTTP_REFERER]
-		set ins(ip_create)		[env REMOTE_ADDR]
-		set ins(ip_recent)		$ins(ip_create)
-		set ins(agent_create)	[env HTTP_USER_AGENT]
-		set ins(agent_recent)	$ins(agent_create)
+	proc page_response {} {
+		uplevel 1 {
+			unset -nocomplain response
+			load_response
 
-		if {[sql_insert_from_array sessions ins]} {
-			cookie set ergkeeper_session $ins(session) -path "/" -days 3650
+			if {![array exists response]} {
+				array set response {}
+			}
 		}
-
-		unset -nocomplain ::session
-		pg_select $::db "SELECT * FROM sessions WHERE session = [pg_quote $ins(session)]" buf {
-			return [array get buf {[a-z]*}]
-		}
-	}
-
-	return {session ""}
-}
-
-proc update_session {id} {
-	set sql "UPDATE sessions SET ip_recent = [pg_quote [env REMOTE_ADDR]], agent_recent = [pg_quote [env HTTP_USER_AGENT]] WHERE session = [pg_quote $id]"
-	sql_exec $::db $sql
-}
-
-proc require_login {} {
-	if {![info exists ::user(id)]} {
-		puts [runkeeper_login_button]
-		page_foot
-		page_term
 	}
 }
 
@@ -177,6 +179,8 @@ proc rowclass {prefix {id "default"}} {
 proc head {buf {level 2}} {
 	return "<h$level>$buf</h$level>"
 }
+
+
 
 package provide ergkeeper 1.0
 
