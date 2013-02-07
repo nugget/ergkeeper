9933d6fed259d81fefc5b94b0b7f66d85dc9456d
David McNett <nugget@flightaware.com>
2013-02-06 22:20:42 -0600

    Include google analytics code.

--- packages/ergkeeper/web.tcl
+++ packages/ergkeeper/web.tcl
@@ -67,7 +67,12 @@ namespace eval ::ergkeeper {
 		}
 
 		# puts "<div class=\"footer\">&copy; Copyright 2012 David C. McNett.  All Rights Reserved.</div>"
+
 		puts "</body>"
+
+		if {[info exists ::config(analytics_account)]} {
+			puts [analytics_javascript]
+		}
 		puts "</html>"
 	}
 
@@ -186,6 +191,35 @@ namespace eval ::ergkeeper {
 		}
 
 		sql_insert_from_array site_errors ins
+	}
+
+
+	proc analytics_javascript {} {
+		set retbuf ""
+
+		append retbuf {<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '}
+
+		append retbuf $::config(analytics_account)
+
+		append retbuf {']);
+			_gaq.push(['_setDomainName', '}
+
+		append retbuf [apache_info virtual]
+
+		append retbuf {']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+			</script>
+		}
+
+		return $retbuf
 	}
 }
 
